ef45383aa46c5a39fee623342780fd53209eecc5
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-01-17 23:11:02 +0530

    Update second_largest_element_of_array.cpp

--- arrays/second_largest_element_of_array.cpp
+++ arrays/second_largest_element_of_array.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int secondSmallest(int arr[],int n){
     if(n<2)
         return -1;
+	
     int small = INT_MAX;
     int second_small = INT_MAX;
     int i;
+	
     for(i = 0; i < n; i++) 
     {
        if(arr[i] < small)
@@ -25,16 +27,16 @@ int secondSmallest(int arr[],int n){
 int secondLargest(int arr[],int n){
 	if(n<2)
 	return -1;
+	
     int large=INT_MIN,second_large=INT_MIN;
     int i;
-    for (i = 0; i < n; i++) 
-    {
+	
+    for (i = 0; i < n; i++){
         if (arr[i] > large) 
         {
             second_large = large;
             large = arr[i];
         }
- 
         else if (arr[i] > second_large && arr[i] != large) 
         {
             second_large = arr[i];
