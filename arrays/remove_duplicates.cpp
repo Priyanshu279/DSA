7b927f9541de9ab15d35075d938960853b64fa62
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-02-12 21:50:21 +0530

    Update remove_duplicates.cpp

--- arrays/remove_duplicates.cpp
+++ arrays/remove_duplicates.cpp
@@ -12,7 +12,7 @@ int removeDuplicates(int arr[], int n){
   return i + 1;
 }
 int main() {
-  int arr[] = {1,1,2,2,2,3,3};
+  int arr[] = {1,1,2,2,2,3,3,4,5,6,6};
   int n = sizeof(arr)/sizeof(arr[0]);
   int k = removeDuplicates(arr, n);
   cout << "The array after removing duplicate elements is " << endl;
